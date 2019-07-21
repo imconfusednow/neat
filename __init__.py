03fd77a31e371c86b551d1628aa6f63c936ac87e
Ben Allport <ben_e_allport@hotmail.co.uk>
2019-07-21 20:27:13 +0100

    commit of largely working neat, from xor dir

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from population import Population
+from network import Network
+from dataconf import DataConf
+from species import Species
+from neuron import Neuron
+from connection import Connection
